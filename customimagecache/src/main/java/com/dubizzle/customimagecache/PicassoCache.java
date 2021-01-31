8498c1dc51cefe9b523a97d0af0280f31172fd83
SWI\arslan.sohail <arslan.sohail@aircod.com>
2021-01-31 15:57:10 +0500

    no message

--- customimagecache/src/main/java/com/dubizzle/customimagecache/PicassoCache.java
+++ customimagecache/src/main/java/com/dubizzle/customimagecache/PicassoCache.java
@@ -1,8 +1,7 @@
 package com.dubizzle.customimagecache;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.util.TypedValue;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.NetworkPolicy;
@@ -11,11 +10,14 @@ import com.squareup.picasso.Picasso;
 public class PicassoCache {
 
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     public static void load(String url, ImageView view, int height, int width) {
 
         Picasso
                 .get()
                 .load(url)
+                .placeholder(view.getResources().getDrawable(R.drawable.emptyholder))
+                .transform(new RoundTransform(5,5))
                 .networkPolicy(NetworkPolicy.OFFLINE)
                 .resize((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width, view.getContext().getResources().getDisplayMetrics())
                         ,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height, view.getContext().getResources().getDisplayMetrics()))
@@ -32,6 +34,8 @@ public class PicassoCache {
                                         Picasso
                                                 .get()
                                                 .load(url)
+                                                .placeholder(view.getResources().getDrawable(R.drawable.emptyholder))
+                                                .transform(new RoundTransform(5,5))
                                                 .resize((int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width, view.getContext().getResources().getDisplayMetrics())
                                                         ,(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height, view.getContext().getResources().getDisplayMetrics()))
                                                 .into(view, new com.squareup.picasso.Callback() {
